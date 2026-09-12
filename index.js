69d84a7a01f60cf8cc4666398f1a48d037a376fd
ArpitGoel1602 <arpitgoel094@gmail.com>
2026-09-12 12:42:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-interview-analysis-platform-frontend.onrender.com",
     credentials: true
 }))
 
